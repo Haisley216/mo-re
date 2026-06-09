1663328869cbfb5defb6045d2a5a5e144f0e4373
Kim Gahyeon <gimgahyeon@gimgahyeon-ui-MacBookAir.local>
2026-06-10 00:55:25 +0900

    update: remove mixpanel debug mode

--- src/lib/mixpanel.ts
+++ src/lib/mixpanel.ts
@@ -3,7 +3,6 @@ import mixpanel from 'mixpanel-browser';
 mixpanel.init(import.meta.env.VITE_MIXPANEL_TOKEN as string, {
   track_pageview: false,
   persistence: 'localStorage',
-  debug: true,
 });
 
 export default mixpanel;
